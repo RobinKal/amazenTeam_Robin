c98a312d3a0cfc96197597b437fe60269378b1cb
robin kalck <fisher9g@gmail.com>
2022-06-16 13:49:54 +0200

    Models for product list view, details

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -1,5 +1,24 @@
 @extends('layout')
 
 @section('content')
-    <h1>Fiche du produit {{$id}}</h1>
+
+    @if(isset($product))
+
+
+
+        <div class="container-full row ptblr-5 d-flex justify-content-around">
+            <h1>Details of {{$product->name}}</h1>
+            <div class="col-2 m-1 background">
+                <h3 class="text-center">{{$product->name}}</h3>
+                <p>{{$product->description}}</p>
+                <p>{{$product->weight}} Gr</p>
+                <img src="{{$product->url_image}}" width="80" height="80">
+                <p>{{$product->price / 100}} €</p>
+            </div>
+        </div>
+
+    @else
+        <h1>Le produit n'est pas valide</h1>
+    @endif
+
 @endsection
